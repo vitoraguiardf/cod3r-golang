7ea90858716630b82d6dd8fbb5d5b46ce3e66e5b
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-16 13:50:51 -0300

    logicos

---
+++ fundamentos/logicos/logicos.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func compras(trab1, trab2 bool) (bool, bool, bool) {
+	comprarTv50 := trab1 && trab2
+	comprarTv32 := trab1 != trab2
+	tomarSorvete := trab1 || trab2
+	return comprarTv50, comprarTv32, tomarSorvete
+}
+
+func main() {
+	tv50, tv32, sorvete := compras(true, true)
+
+	fmt.Printf("Tv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t", tv50, tv32, sorvete, !sorvete)
+}
