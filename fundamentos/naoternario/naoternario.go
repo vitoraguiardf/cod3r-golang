9ce58b2490abd272f3878f0954b4d5d919d7458e
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-16 22:26:05 -0300

    nao ternario main

--- fundamentos/naoternario/naoternario.go
+++ fundamentos/naoternario/naoternario.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Em Golang, não existe operador ternário
 func obterResultado(nota float64) string {
 	// A seguinte expressão comentada é inválida
@@ -8,4 +10,10 @@ func obterResultado(nota float64) string {
 		return "Aprovado"
 	}
 	return "Reprovado"
+}
+func main() {
+	fmt.Println("Julia", obterResultado(6.2))
+	fmt.Println("Paulo", obterResultado(6.0))
+	fmt.Println("Clara", obterResultado(5.2))
+	fmt.Println("Roberto", obterResultado(8.1))
 }
