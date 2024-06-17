07f24d84dfee93483165d3709adbd77daca51a33
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-17 16:14:28 -0300

    Comentando

--- 02 - controles/ifelse/ifelse.go
+++ 02 - controles/ifelse/ifelse.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func imprimirResultado(nota float64) {
+	// Não necessita de parêntesis, e as chaves são obrigatórias
 	if nota >= 7 {
 		fmt.Println("Aprovado com nota", nota)
 	} else {
@@ -13,4 +14,6 @@ func imprimirResultado(nota float64) {
 func main() {
 	imprimirResultado(7.3)
 	imprimirResultado(5.1)
+	imprimirResultado(4.9)
+	imprimirResultado(8.1)
 }
