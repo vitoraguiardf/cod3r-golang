313bb9020c52d93b6a95ebed302a52e01c4c5590
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-18 19:26:55 -0300

    forange updated

--- 03 - array slice map/forrange/forrange.go
+++ 03 - array slice map/forrange/forrange.go
@@ -5,6 +5,16 @@ import "fmt"
 func main() {
 	numeros := [...]int{1, 2, 3, 4, 5}
 	for i, numero := range numeros {
-		fmt.Printf("%d) %d", i+1, numero)
+		fmt.Printf("%d) %d\n", i, numero)
+	}
+
+	fmt.Println("Acessando somente indexes")
+	for idx := range numeros {
+		fmt.Println(idx)
+	}
+
+	fmt.Println("Acessando somente valores")
+	for _, num := range numeros {
+		fmt.Println(num)
 	}
 }
